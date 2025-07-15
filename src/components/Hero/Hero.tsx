63af132d587e0ab8a01e161c1edeaade8bfe1e4c
moatasim <mo32000a@gmail.com>
2025-07-15 21:45:32 +0300

    added hero slider

--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -4,6 +4,14 @@ import styles from "./hero.module.css"
 // import { useTranslations } from 'next-intl';
 // import MainBtn from '../MainBtn/MainBtn';
 import Logo from '../Logo/logo';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { Autoplay, EffectFade, Navigation, Pagination } from 'swiper/modules';
+import 'swiper/css';
+import "swiper/css/bundle";
+import 'swiper/css/navigation'; 
+import 'swiper/css/pagination'; 
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faChevronLeft, faChevronRight } from '@fortawesome/free-solid-svg-icons';
 const Hero = ({
     lo
 }: {
@@ -27,46 +35,69 @@ const Hero = ({
 
   return (
     <section id='hero' className={classNames.join(" ")}>
-        <div className={styles.part}>
-            <div className={styles.logo}>
-                <Logo lo={lo} logoNo={1}></Logo>
+        <div className={styles.swiper}>
+            <Swiper
+                loop
+                autoplay={{
+                    delay: 5000
+                }}
+                slidesPerView= {1}
+                spaceBetween={0}
+                pagination={{
+                    clickable: true,
+                    el: `.heroPagination`
+                }}
+                modules={[Navigation, Pagination, Autoplay, EffectFade]}
+                navigation={{
+                    nextEl: '.featured-swiper-button-next',
+                    prevEl: '.featured-swiper-button-prev'
+                }}
+                effect='fade'
+                className={styles.mySwiperProdHero}
+                // autoHeight={true}
+                >
+                {/* <SwiperSlide className={styles.first}>
+                    <div className={styles.img}>
+                        <Image src={logo} alt={`Al-assema for compressed air systems logo`}></Image>
+                    </div>
+                    <p>
+                        {t("0")}
+                    </p>
+                </SwiperSlide> */}
+                <SwiperSlide className={styles.part + " " + styles.one} id='one'>
+                        <div className={styles.logo}>
+                            <Logo lo={lo} logoNo={1}></Logo>
+                        </div>
+                        <h1>
+                            ICSF
+                        </h1>
+                        <p>
+                            
+                            born in 2000 an have around 24 years’ experience on fire fighting and alarm systems sector
+                        </p>
+                </SwiperSlide>
+                <SwiperSlide className={styles.part + " " + styles.two} id='two'>
+                        <div className={styles.logo}>
+                            <Logo lo={lo} logoNo={2}></Logo>
+                        </div>
+                        <h1>
+                            Fire Link
+                        </h1>
+                        <p>
+                                born in 2024 as sister company for international  
+                                center for safety and fire.
+                        </p>
+                </SwiperSlide>
+            </Swiper>
+            <div className={styles.heroPagination}>
+                <div className={`heroPagination`}></div>
             </div>
-            <h1>
-                {/* {
-                    t("Heading")
-                } */}
-                ICSF
-            </h1>
-            <p>
-                {/* {
-                    t("P")
-                } */}
-                born in 2000 an have around 24 years’ experience on fire fighting and alarm systems sector
-            </p>
-            {/* <MainBtn href="#contact" lo={lo}>
-                {t("MainBtn")}
-            </MainBtn> */}
-        </div>
-        <div className={styles.part}>
-            <div className={styles.logo}>
-                <Logo lo={lo} logoNo={2}></Logo>
-            </div>
-            <h1>
-                {/* {
-                    t("Heading")
-                } */}
-                Fire Link
-            </h1>
-            <p>
-                    {/* {
-                        t("P")
-                    } */}
-                    born in 2024 as sister company for international  
-                    center for safety and fire.
-            </p>
-            {/* <MainBtn href="#contact" lo={lo}>
-                {t("MainBtn")}
-            </MainBtn> */}
+            <button className='featured-swiper-button-prev hexagon'>
+                <FontAwesomeIcon icon={faChevronLeft} />
+            </button>
+            <button className='featured-swiper-button-next hexagon'>
+                <FontAwesomeIcon icon={faChevronRight} />
+            </button>
         </div>
     </section>
   )
