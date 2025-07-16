138552548f8f82840405236acc6117a54d4ef9a1
moatasim <mo32000a@gmail.com>
2025-07-16 19:44:47 +0300

    sliders break points 2

--- src/components/Partners/Partners.tsx
+++ src/components/Partners/Partners.tsx
@@ -50,13 +50,13 @@ const Partners = ({
                 breakpoints={{
                     400: {
                     slidesPerView: 3,
-                    spaceBetween: 10
+                    // spaceBetween: 10
                     },
                     640: {
-                    slidesPerView: 2,
+                    slidesPerView: 3,
                     },
                     768: {
-                    slidesPerView: 3,
+                    slidesPerView: 4,
                     },
                     1024: {
                     slidesPerView: 5,
